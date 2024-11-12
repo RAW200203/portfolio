4feaebf23e0ea9cf5d7da05c6db3fcdd6fb4a1f0
RAW200203 <randyachana2002@gmail.com>
2024-11-12 16:04:20 +0100

    3 webpages done

--- business-landing-page/script.js
+++ business-landing-page/script.js
@@ -1,1 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Thank you for contacting us! We'll get back to you soon.");
+});
 
